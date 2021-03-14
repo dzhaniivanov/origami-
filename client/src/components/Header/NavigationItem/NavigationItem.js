378a16adf9355e5bc5fc2ff17f062077e7870687
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-03-14 17:44:54 +0200

    extract nav item from header

---
+++ client/src/components/Header/NavigationItem/NavigationItem.js
@@ -0,0 +1,13 @@
+import style from './NavigationItem.module.css';
+
+const NavigationItem = (props) => {
+    return (
+        <li className="listItem">
+            <a href="#" className={style.navListItem}>{props.children}</a>
+        </li>
+
+    );
+
+}
+
+export default NavigationItem;
